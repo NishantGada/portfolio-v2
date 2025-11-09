504bb356bde252b8f95d8b77411bcab855d114a3
NishantGada <nishant.gada160@gmail.com>
2025-11-09 17:59:40 -0500

    setup: creates initial data file for experiences

---
+++ src/utils/data/experience.js
@@ -0,0 +1,57 @@
+export const experiences = [
+  {
+    title: "Software Engineering Co-op",
+    company: "Hometap",
+    duration: "January 2025 to June 2025",
+    items: [
+      "Engineered core LMS migration services to third-party integrations using Python, Django REST Framework, React, and TypeScript to boost system reliability by 29% and user growth by 15%",
+      "Developed CI/CD pipelines for staged deployments across 5+ cross-functional teams, cutting release time by 20% with less than 2% post-release issues",
+      "Contributed towards onboarding 3 new partners onto the Home Equity Dashboard to further support customer loan journey and management",
+      "Drove static typing adoption using TypeScript and Pydantic, ensured code quality via 100+ code reviews, and introduced reusable design patterns",
+      "Leveraged test-driven development to increase test coverage from 73% to 86% by adding unit tests, integration tests, and end-to-end tests using Pytest, Jest, and Cypress, preventing regression failures"
+    ]
+  },
+  {
+    title: "Software Engineer",
+    company: "Think360.ai (Think Analytics)",
+    duration: "September 2021 to June 2023",
+    items: [
+      "Launched a Regulatory Audit Dashboard handling 3M+ customer records under strict government compliance, cutting service response time by 22%",
+      "Bumped revenue by 27% by improving system performance using domain-driven design, Redis caching, and Python-based code optimizations",
+      "Built a secure, high-throughput KYC microservice using Python, Flask, SQL, and AWS–reducing identity verification time and processing delays by 23%",
+      "Boosted client funnel by 36% scaling the microservice across 3 major business verticals with the help of Docker, GitHub Actions, and Kafka",
+      "Led bi-weekly demos across product, engineering, and design teams to align delivery timelines, preempt release bottlenecks, and ensure seamless go-lives",
+      "Delivered a full-stack Loan Disbursal System (Python, Java, React, Redis, SQL/NoSQL, AWS) for 50k+ customers, launching an MVP within 4 agile sprints"
+    ]
+  },
+  {
+    title: "Associate Software Engineer",
+    company: "Accenture",
+    duration: "June 2021 to September 2021",
+    items: [
+      "Architected a cross - platform health analytics application using Python, Flask, GCP, and React Native; served 75K+ users and enabled real- time tracking of health metrics across Android and iOS applications",
+      "Optimized SQL query response time from ~112 seconds to ~6 seconds through strategic indexing and optimization techniques, significantly improving data retrieval speed and overall user experience"
+    ]
+  },
+  {
+    title: "Full Stack Engineering Intern",
+    company: "Nirol",
+    duration: "April 2020 to May 2020",
+    items: [
+      "Designed a TikTok style video sharing social network while managing a team of 5 interns using Python, Django, GraphQL, React, Redux",
+      "Established core frontend and backend features via asynchronous Django services and Redux state management to track and display likes,comments, and follow / unfollow events to improve user interaction by 47%",
+      "Wrote and optimized 20+ GraphQL APIs for user relationships and recommendation pipelines, leveraging data prefetching and query batching"
+    ]
+  },
+  {
+    title: "Research and Programming Intern",
+    company: "University of Mumbai",
+    duration: "July 2019 to December 2019",
+    items: [
+      "Developed a robust Python pipeline to fetch and analyze thousands of tweets using the Twitter API, focusing on public reactions to the revocation of Article 370",
+      "Performed advanced sentiment analysis using Python and NLP techniques, extracting meaningful insights from unstructured social media data.",
+      "Created compelling visualizations with Matplotlib, highlighting trends and patterns in public opinion with clarity and precision",
+      "Identified prominent thought leaders and influencers, providing a detailed assessment of citizens’ positive and negative attitudes toward the government’s action"
+    ]
+  },
+];
