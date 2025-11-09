e56aea8c9182e0be485565f8d66146ef148dc080
NishantGada <nishant.gada160@gmail.com>
2025-11-09 18:36:31 -0500

    refactor: exports project data into a file and maps over it + implements show project feature

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,58 +1,30 @@
 import React from 'react';
 import './Projects.css';
+import { projects } from '../../utils/data/projects';
 
 export default function Projects() {
   return (
-    <section class="projects" id="projects">
+    <section className="projects" id="projects">
       <h2>Projects</h2>
-      <div class="grid-container">
-        <div class="project" id="project1">
-          <h2>ComfortZone <span>🛠️</span></h2>
-          <p><i>Tech Stack: Python, Flask, MySQL</i></p>
-          <p>
-            A project that I think would be relevant to many. This is a toilet aggregator/ searching app that
-            would allow the user to - find a nearby toilet, rate it on the application, add its pictures, and further
-            information. This would allow for better accessibility to toilets, comfort, and hygiene for everyone.
-          </p>
-          <a class="github-links" href="https://github.com/NishantGada/comfort-zone-flask-backend" target="_blank">View on
-            Github</a>
-        </div>
-        <div class="project" id="project2">
-          <h2>Jiraffe</h2>
-          <p><i>Tech Stack: Java, Spring Boot, Hibernate, SQL</i></p>
-          <p>
-            Developed a bug tracking application with a user-friendly JSP interface, implementing role-based
-            authorization to streamline bug reporting, assignment, and tracking while reducing unauthorized
-            actions. Leveraged Hibernate ORM and DAO patterns for efficient data management and real-time
-            updates, and designed an advanced dashboard with filtering and sorting capabilities.
-          </p>
-          <a class="github-links" href="https://github.com/NishantGada/bug-tracker" target="_blank">View on
-            Github</a>
-        </div>
-        <div class="project" id="project3">
-          <h2>BostonTown</h2>
-          <p><i>Tech Stack: React, Node, Express, MongoDB</i></p>
-          <p>
-            Led a team of 4 to develop a Boston-based listing website using the MERN stack, featuring over
-            300,000 properties for improved apartment search and connecting users and agents conveniently.
-            Designed a user reporting feature with an interactive heat map, for increased trust and informed
-            decision-making.
-          </p>
-          <a class="github-links" href="https://github.com/NishantGada/BostonTown" target="_blank">View on
-            Github</a>
-        </div>
-        <div class="project" id="project4">
-          <h2>Unwritten</h2>
-          <p><i>Tech Stack: Python, Django, JavaScript, CSS, Tailwind CSS, SQL</i></p>
-          <p>
-            Developed a full-stack blogging platform with a robust backend supporting blog creation, media
-            uploads, and timestamp tracking. Designed advanced search and filtering features with improving
-            navigation and content discovery.
-          </p>
-          <a class="github-links" href="https://github.com/NishantGada/unwritten" target="_blank">View on
-            Github</a>
-        </div>
+      <div className="grid-container">
+        {projects.map((project, index) => (
+          project.show ? <div className="project" key={index}>
+            <h2>
+              {project.name} {project.emoji && <span>{project.emoji}</span>}
+            </h2>
+            <p><i>Tech Stack: {project.techStack}</i></p>
+            <p>{project.description}</p>
+            <a
+              className="github-links"
+              href={project.githubLink}
+              target="_blank"
+              // rel="noopener noreferrer"
+            >
+              View on Github
+            </a>
+          </div> : <></>
+        ))}
       </div>
     </section>
-  )
+  );
 }
---
+++ src/utils/data/projects.js
@@ -0,0 +1,56 @@
+export const projects = [
+  {
+    name: "QuickSplit [w.i.p]",
+    // emoji: "🛠️",
+    techStack: "Python, FastAPI, Pydantic, JWT Auth, React, JavaScript, TypeScript, Ant Design",
+    description:
+      "A complete, full-stack, end-to-end expense splitting application/web app",
+    githubLink: "https://github.com/NishantGada/quick-split-v2",
+    show: true
+  },
+  {
+    name: "Byte Blogs [w.i.p]",
+    emoji: "🛠️",
+    techStack: "Node.js, Express, JavaScript, TypeScript, React, NoSQL / AWS DynamoDB, Chakra UI, Vite",
+    description:
+      "A simple, second, personal blogging platform I am developing to further expand my skills, technical depth, use AI more efficiently, and this time hopefully actually write something.",
+    githubLink: "https://github.com/NishantGada/byte-blogs",
+    show: true
+  },
+  {
+    name: "ComfortZone",
+    emoji: "🛠️",
+    techStack: "Python, Flask, MySQL",
+    description:
+      "A project that I think would be relevant to many. This is a toilet aggregator/searching app that allows the user to find a nearby toilet, rate it, add pictures, and further information. This improves accessibility, comfort, and hygiene for everyone.",
+    githubLink: "https://github.com/NishantGada/comfort-zone-flask-backend",
+    show: false
+  },
+  {
+    name: "Jiraffe",
+    emoji: "",
+    techStack: "Java, Spring/Spring Boot, Hibernate, SQL",
+    description:
+      "Developed a bug tracking application with a user-friendly JSP interface, implementing role-based authorization to streamline bug reporting, assignment, and tracking. Leveraged Hibernate ORM and DAO patterns for efficient data management and real-time updates, and designed an advanced dashboard with filtering and sorting capabilities.",
+    githubLink: "https://github.com/NishantGada/bug-tracker",
+    show: true
+  },
+  {
+    name: "BostonTown",
+    emoji: "",
+    techStack: "React, Node.js, Express, MongoDB, Ant Design",
+    description:
+      "Led a team of 4 to develop a Boston-based listing website using the MERN stack, featuring over 300,000 properties for improved apartment search. Designed a user reporting feature with an interactive heat map for increased trust and informed decision-making.",
+    githubLink: "https://github.com/NishantGada/BostonTown",
+    show: true
+  },
+  {
+    name: "Unwritten",
+    emoji: "",
+    techStack: "Python, Django, JavaScript, CSS, Tailwind CSS, SQL",
+    description:
+      "Developed a full-stack blogging platform with a robust backend supporting blog creation, media uploads, and timestamp tracking. Designed advanced search and filtering features to improve navigation and content discovery.",
+    githubLink: "https://github.com/NishantGada/unwritten",
+    show: true
+  }
+];
