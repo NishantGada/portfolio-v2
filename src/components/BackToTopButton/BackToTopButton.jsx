4edfae30d13173a0756d24212ef5c21cdae2b742
NishantGada <nishant.gada160@gmail.com>
2025-11-09 17:59:23 -0500

    refactor: back-to-top button

--- src/components/BackToTopButton/BackToTopButton.jsx
+++ src/components/BackToTopButton/BackToTopButton.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './BackToTopButton.css';
 
 export default function BackToTopButton() {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      setVisible(window.scrollY > 300);
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    if (window.history.replaceState) {
+      window.history.replaceState(null, null, window.location.href);
+    }
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
   return (
-    <div class="back-to-top">
-      <a href="#nav"><i class="fa fa-arrow-up" aria-hidden="true"></i></a>
+    <div className={`back-to-top ${visible ? 'active' : ''}`}>
+      <a href="#nav" aria-label="Back to top">
+        <i className="fa fa-arrow-up" aria-hidden="true"></i>
+      </a>
     </div>
-  )
+  );
 }
