77468a1ed2c7cfd7d3165eec378ba2cffd80a8ad
NishantGada <nishant.gada160@gmail.com>
2025-11-09 17:44:48 -0500

    chore: App.jsx import ordering refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import Navbar from './sections/Navbar/Navbar'
-import Banner from './sections/Banner/Banner'
-import About from './sections/About/About'
-import Skills from './sections/Skills/Skills'
-import Experience from './sections/Experience/Experience'
-import Projects from './sections/Projects/Projects'
-import Education from './sections/Education/Education'
-import Contact from './sections/Contact/Contact'
 import BackToTopButton from './components/BackToTopButton/BackToTopButton'
+import About from './sections/About/About'
+import Banner from './sections/Banner/Banner'
+import Contact from './sections/Contact/Contact'
+import Education from './sections/Education/Education'
+import Experience from './sections/Experience/Experience'
+import Navbar from './sections/Navbar/Navbar'
+import Projects from './sections/Projects/Projects'
+import Skills from './sections/Skills/Skills'
 
 function App() {
   return (
