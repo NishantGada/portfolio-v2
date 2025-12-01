17c92c16e59372e184ed9b5378989b7113daf896
NishantGada <nishant.gada160@gmail.com>
2025-11-30 19:46:15 -0500

    refactor(Contact): code cleanup

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -8,31 +8,34 @@ export default function Contact() {
         <h1>Contact</h1>
       </div>
 
-      <div id="reach_me_email">
-        <p><a href="">gada.ni@northeastern.edu</a></p>
+      <div id="email">
+        <a href="">gada.ni@northeastern.edu</a>
       </div>
 
       <div id="sub-contact">
-        <p>
+        <div className="socials">
           <span>
             <a href="https://www.instagram.com/nishantgada_/" target="_blank">
-              <i class="fa fa-instagram" aria-hidden="true" />
+              <i class="fa fa-instagram contact-icons" aria-hidden="true" />
             </a>
           </span>
           <span>
             <a href="https://www.linkedin.com/in/nishant-gada/" target="_blank">
-              <i class="fa fa-linkedin" aria-hidden="true"></i>
+              <i class="fa fa-linkedin contact-icons" aria-hidden="true"></i>
             </a>
           </span>
           <span>
             <a href="https://github.com/NishantGada/" target="_blank">
-              <i class="fa fa-github" aria-hidden="true"></i>
+              <i class="fa fa-github contact-icons" aria-hidden="true"></i>
             </a>
           </span>
-        </p >
+        </div>
+      </div >
+
+      <div className="location">
         <p>Boston</p>
         <p>MA, USA.</p>
-      </div >
+      </div>
     </section >
   )
 }
