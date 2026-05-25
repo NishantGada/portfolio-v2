4531a947c955b369744feec42474a9b59a71ace1
NishantGada <nishant.gada160@gmail.com>
2026-05-24 21:29:04 -0400

    update skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -9,40 +9,47 @@ export default function Skills() {
       <div className="content">
         <div className="skill">
           <h2>Languages</h2>
-          <p>C, C#, C++, Go/Golang</p>
+          <p>Python, JavaScript, TypeScript, Java, C#, C++</p>
         </div>
         <div className="skill">
           <h2>Front End</h2>
-          <p>React, TypeScript, JavaScript, Angular, HTML, Next.js</p>
+          <p>React, TypeScript, Angular, Next.js</p>
           <p>CSS, SCSS, Tailwind CSS, Ant Design, Material UI, Chakra UI, Shadcn, Figma</p>
         </div>
         <div className="skill">
           <h2>Back end</h2>
-          <p>Python, Django REST Framework, Flask, FastAPI</p>
-          <p>JavaSE, JavaEE, Spring, Spring Boot</p>
+          <p>Django/Django REST Framework, Flask, FastAPI</p>
+          <p>Spring, Spring Boot</p>
           <p>ASP.NET Core</p>
           <p>Nest.js, Node.js, Express</p>
         </div>
         <div className="skill">
           <h2>Mobile App Developement</h2>
-          <p>React-Native, Flutter, Kotlin, Jetpack</p>
+          <p>React Native, Expo, Kotlin, Jetpack</p>
         </div>
         <div className="skill">
           <h2>Cloud</h2>
-          <p>Amazon Web Services, Google Cloud Platform, Heroku, Netlify, Vercel</p>
+          <p>AWS - Amazon Web Services</p>
+          <p>GCP - Google Cloud Platform</p>
+          <p>Microsoft Azure</p>
+          <p>Heroku, Netlify, Vercel</p>
         </div>
         <div className="skill">
           <h2>Database</h2>
-          <p>SQL (MySQL, PostgreSQL)</p>
-          <p>NoSQL (MongoDB, Firebase, Redis)</p>
+          <p>SQL - MySQL, PostgreSQL, SQL Server (MSSQL)</p>
+          <p>NoSQL - MongoDB, Firebase</p>
         </div>
         <div className="skill">
           <h2>DevOps, CI/CD, and Infra</h2>
           <p>Docker, Kubernetes, Jenkins, GitHub Actions, Terraform, Packer, Unix/Linux</p>
         </div>
         <div className="skill">
+          <h2>Distributed Systems</h2>
+          <p>Kafka, RabbitMQ, Redis, Elasticsearch</p>
+        </div>
+        <div className="skill">
           <h2>Leadership</h2>
-          <p>Communication, Vision, Management, and Belief. </p>
+          <p>Communication, Vision, Management, and Belief</p>
         </div>
       </div>
     </section>
